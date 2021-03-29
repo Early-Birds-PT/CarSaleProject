c35e506e16423d451a6f104fefa7451f4b4a02f4
codergo7 <codergo7@gmail.com>
2021-03-29 12:44:00 +0200

    rearranged the structure of the project, added embeddable package

---
+++ backend/src/main/java/model/entity/Employee.java
@@ -0,0 +1,28 @@
+package model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import model.embeddable.Person;
+
+import javax.persistence.*;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int employeeNumber;
+    private String extension;
+    private String officeCode;
+    private int reportsTo;
+    private String jobTitle;
+
+    @Embedded
+    private Person person;
+}
