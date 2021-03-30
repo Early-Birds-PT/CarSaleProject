25826ff7025cdec80d5bcd41fb7c14cae26f7fb0
Apple <ellinemedrano1408@hotmail.com>
2021-03-30 10:45:23 +0200

    added office and employee repositories

---
+++ backend/src/main/java/repository/OfficeRepository.java
@@ -0,0 +1,12 @@
+package repository;
+
+
+import data.model.entity.Office;
+
+public interface OfficeRepository {
+
+    Office createOffice(Office office);
+    Office readOffice(String officeCode);
+    void updateOffice(Office office);
+    void deleteOffice(Office office);
+}
