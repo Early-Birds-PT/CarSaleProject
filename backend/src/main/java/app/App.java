183b4673c6861874363321bc429cc97ea701900b
codergo7 <codergo7@gmail.com>
2021-03-31 01:17:17 +0200

    test read methods

--- backend/src/main/java/app/App.java
+++ backend/src/main/java/app/App.java
@@ -1,19 +1,30 @@
 package app;
 
-import data.model.entity.Customer;
-import data.model.entity.Order;
-import data.model.entity.Payment;
-import service.CustomerService;
-import service.PaymentService;
-import service.impl.CustomerServiceImpl;
-import service.OrderService;
-import service.impl.OrderServiceImpl;
-import service.impl.PaymentServiceImpl;
+import data.model.embeddable.OrderProductPK;
+import data.model.entity.*;
+import service.*;
+import service.impl.*;
 
 public class App {
 
     public static void main(String[] args) {
+
         OrderService orderService = new OrderServiceImpl();
+        OrderDetailService orderDetailService = new OrderDetailServiceImpl();
+        Order order = orderService.readOrder(10100);
+        ProductService productService = new ProductServiceImpl();
+        Product product = productService.readProduct("S18_1749");
+
+        System.out.println(order);
+        System.out.println(product);
+
+        OrderProductPK orderProductPK = new OrderProductPK(order,product);
+
+        OrderDetail orderDetails = orderDetailService.readOrderDetails(orderProductPK);
+        System.out.println(orderDetails);
+
+
+       /* OrderService orderService = new OrderServiceImpl();
         Order order = orderService.readOrder(10112);
         System.out.println(order);
 
@@ -24,6 +35,8 @@ public class App {
         PaymentService paymentService = new PaymentServiceImpl();
         Payment payment = paymentService.readPayment("HQ336336");
         System.out.println(payment);
+
+        */
 
     }
 }
