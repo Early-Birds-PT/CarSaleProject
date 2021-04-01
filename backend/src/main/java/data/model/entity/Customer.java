caeb294f3af7cdb730ce7414246955cb7551e4ed
codergo7 <codergo7@gmail.com>
2021-04-01 10:18:32 +0200

    clean code

--- backend/src/main/java/data/model/entity/Customer.java
+++ backend/src/main/java/data/model/entity/Customer.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -28,17 +27,7 @@ public class Customer {
     private int salesRepEmployeeNumber;
     private BigDecimal creditLimit;
 
-    //bi-directional many-to-one association to Employee
     @ManyToOne
     @JoinColumn(name="salesRepEmployeeNumber",  insertable = false, updatable = false)
     private Employee employee;
-
-    /*//bi-directional many-to-one association to Order
-    @OneToMany(mappedBy="customer",fetch = FetchType.EAGER)
-    private List<Order> orders;*/
-
-    //bi-directional many-to-one association to Payment
-    /*@OneToMany(mappedBy="customer", fetch = FetchType.LAZY)
-    private List<Payment> payments;*/
-
 }
