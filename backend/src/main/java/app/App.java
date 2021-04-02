2c571d0e192dc9cdcf35aebc8350f62a9f3603fa
Mert <mertdemirok80@gmail.com>
2021-04-03 00:38:15 +0200

    delete Program Class and move launching code into App Class

--- backend/src/main/java/app/App.java
+++ backend/src/main/java/app/App.java
@@ -1,19 +1,14 @@
 package app;
 
-
-import data.model.embeddable.PaymentId;
-import data.model.entity.*;
-import service.EmployeeService;
-import service.ProductService;
-import service.impl.EmployeeServiceImpl;
-import utils.ServiceBeanFactory;
-
-import java.math.BigDecimal;
-import java.util.List;
+import view.UserInterface;
+import java.util.Scanner;
 
 public class App {
-
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        UserInterface userInterface = new UserInterface(scanner);
+        userInterface.start();
 
 //        List<Customer> allCustomers= ServiceBeanFactory.getCustomerService().getAllCustomers();
 //        allCustomers.forEach(customer -> System.out.println(customer));
@@ -25,12 +20,12 @@ public class App {
         allProducts.forEach(product -> System.out.println(product));*/
 
 
-        Customer customer = ServiceBeanFactory.getCustomerService().readCustomer(181);
+        /*Customer customer = ServiceBeanFactory.getCustomerService().readCustomer(181);
         List<Order> orderList = ServiceBeanFactory.getOrderService().findAllOrdersByCustomer(customer);
         orderList.forEach(order -> System.out.println(order));
 
         List<Payment> paymentList = ServiceBeanFactory.getPaymentService().findAllPaymentsByCustomer(customer);
-        paymentList.forEach(payment -> System.out.println(payment));
+        paymentList.forEach(payment -> System.out.println(payment));*/
 
         /*Customer customer = ServiceBeanFactory.getCustomerService().readCustomer(103);
         PaymentId paymentId = new PaymentId("OM314933",customer);
@@ -112,8 +107,5 @@ public class App {
 
         // officeService.updateOffice(office1);
 
-
     }
-
-
 }
