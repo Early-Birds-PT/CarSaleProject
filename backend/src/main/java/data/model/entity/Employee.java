b3203e466d13b2927573ac8bf57c2cdfe283d19b
codergo7 <codergo7@gmail.com>
2021-04-04 17:46:08 +0200

    implement showAllEmployeesAsATable method

--- backend/src/main/java/data/model/entity/Employee.java
+++ backend/src/main/java/data/model/entity/Employee.java
@@ -28,15 +28,11 @@ public class Employee {
     @JoinColumn(name = "officeCode")
     private Office office;
 
-   /* @Override
+   @Override
     public String toString() {
 
-        return String.format("| %-14s |  %-34s |  %-15s |  %-16s |  %-18s |  %-32s  |  %-24s  |  %-17s | " +
-                        "%-13s | %-10s | %-12s | %11s | %-15s |", employeeNumber, lastName, firstName,
-                extension, email, jobTitle, (employee != null ) ? employee.getEmployeeNumber() : null), (employee != null ) ? employee.getEmployeeNumber() : null)office.getOfficeCode(), state, postalCode, country, creditLimit,
-                (employee != null ) ? employee.getEmployeeNumber() : null);
+        return String.format("| %-14s |  %-13s |  %-13s |  %-13s |  %-35s |  %-20s  |  %-10s  |  %-10s |", employeeNumber, lastName, firstName,
+                extension, email, jobTitle, ((employee != null ) ? employee.getEmployeeNumber() : null), ((office != null ) ? office.getOfficeCode() : null));
     }
-
-    */
 }
 
